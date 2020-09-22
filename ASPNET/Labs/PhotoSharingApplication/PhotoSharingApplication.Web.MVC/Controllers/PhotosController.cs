77cd4387cfa9b043babb6af110d05f94841be268
Simona Colapicchioni <s.colapicchioni@gmail.com>
2020-09-22 16:01:18 +0200

    Lab01

---
+++ ASPNET/Labs/PhotoSharingApplication/PhotoSharingApplication.Web.MVC/Controllers/PhotosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PhotoSharingApplication.Web.MVC.Models;
+using PhotoSharingApplication.Web.MVC.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoSharingApplication.Web.MVC.Controllers
+{
+    public class PhotosController : Controller
+    {
+        //Photos/AllPhotos
+        public IActionResult AllPhotos() {
+            //get the model
+            PhotosRepository repository = new PhotosRepository();
+            List<Photo> photos = repository.GetPhotos();
+            //return the view with the model in it
+            return View(photos);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPNET/Labs/PhotoSharingApplication/PhotoSharingApplication.Web.MVC/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhotoSharingApplication.Web.MVC.Models
+{
+    public class Photo
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
